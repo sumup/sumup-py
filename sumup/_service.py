368d894de2b75deefae8f9f0efdaaf212c73683c
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-04-16 16:32:53 +0200

    feat: init

---
+++ sumup/_service.py
@@ -0,0 +1,24 @@
+import httpx
+from ._version import __version__
+
+HeaderTypes = dict[str, str]
+
+
+class BaseResource:
+    @staticmethod
+    def version():
+        return __version__
+
+
+class Resource(BaseResource):
+    _client: httpx.Client
+
+    def __init__(self, client: httpx.Client) -> None:
+        self._client = client
+
+
+class AsyncResource(BaseResource):
+    _client: httpx.AsyncClient
+
+    def __init__(self, client: httpx.Client) -> None:
+        self._client = client
