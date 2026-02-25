a00de7beefeb76aa12d3b7ae2803cbda14cca436
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-02-25 11:31:22 +0100

    chore: fixups after code re-generation

--- sumup/__init__.py
+++ sumup/__init__.py
@@ -1,6 +1,5 @@
 from sumup._client import Sumup, AsyncSumup
 from sumup._service import Resource, AsyncResource
 from sumup._exceptions import APIError
-from sumup.types import MerchantAccount
 
 __all__ = ["APIError", "AsyncResource", "AsyncSumup", "MerchantAccount", "Resource", "Sumup"]
